8c293555f9417f75df4b5d08c83294c6846c9247
ManonRagnotti <44577662+ManonRagnotti@users.noreply.github.com>
2019-06-12 10:51:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const updateSizeImg = () => {
-		const img = document.querySelector('.container img');
-    img.style.objectFit = 'cover';
-    img.style.objectPosition = 'center';
-    img.style.width = '100%';
-    img.style.height = '100vh';
+	const img = document.querySelector('.container img');
+	img.style.objectFit = 'cover';
+	img.style.objectPosition = 'center';
+	img.style.width = '100%';
+	img.style.height = '100vh';
 }
 
 updateSizeImg();
